5e4f6f4980e39a82fda3c3460ba7afc4fa2b4b95
Edelberth <98649584+albertjimrod@users.noreply.github.com>
2022-09-14 17:46:00 +0200

    140922 ok all

---
+++ 02 Data_Analisis_Visualization/00_Numpy introduction/Exploring Ebay Car Sales Data v2.mm
@@ -0,0 +1,149 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1653303378323" ID="ID_1629404326" MODIFIED="1653303390293" TEXT="autos">
+<node CREATED="1653303391088" ID="ID_1324749755" MODIFIED="1653303418956" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Basic Exploration Data
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1653388616220" ID="ID_8001855" MODIFIED="1653392649267" TEXT="Basic Exploration Data">
+<icon BUILTIN="info"/>
+<node CREATED="1653303459315" ID="ID_30925168" MODIFIED="1653388955774" TEXT="Cleaning column names ">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1653303470033" ID="ID_1785806189" MODIFIED="1653388959215" TEXT="Initial Exploration Data and cleaning ">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1653303480155" ID="ID_371497840" MODIFIED="1653388962807" TEXT="Columns candidates to be droped">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-4"/>
+<node CREATED="1653303498656" ID="ID_1981086212" MODIFIED="1653391260850" TEXT="Columns that need more investigation.">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-5"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1653303542496" ID="ID_288752405" MODIFIED="1653392649267" TEXT="Numeric data stored as text that needs to be cleaned">
+<icon BUILTIN="full-2"/>
+<node CREATED="1653303556962" ID="ID_929256019" MODIFIED="1653389071319" TEXT="Character deletion and integer conversion">
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="full-1"/>
+</node>
+</node>
+<node CREATED="1653303566614" ID="ID_191180257" MODIFIED="1653392649269" TEXT="What does our dataset look like?">
+<icon BUILTIN="full-3"/>
+<node CREATED="1653303578651" ID="ID_876401914" MODIFIED="1653389134583" TEXT="Exploring the odometer_km">
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1653303586608" ID="ID_770221302" MODIFIED="1653303588764" TEXT="Upper limit odometer_km"/>
+<node CREATED="1653303594054" ID="ID_1990039360" MODIFIED="1653303596172" TEXT="Lower limit odometer_km"/>
+</node>
+<node CREATED="1653303605743" ID="ID_1263702422" MODIFIED="1653389167423" TEXT="Exploring the price_in_dollar">
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1653303617160" ID="ID_900438211" MODIFIED="1653389794058" TEXT="Upper limit price_in_dollars">
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1653303625066" ID="ID_640375584" MODIFIED="1653389638251">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Exploring price outliers on the top of the list:
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1653312627321" ID="ID_826580446" MODIFIED="1653312709739">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      How much is our Maseratti worth?
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1653312730121" ID="ID_1116488235" MODIFIED="1653312751506" TEXT="removing from top list"/>
+</node>
+</node>
+<node CREATED="1653303625066" ID="ID_971954479" MODIFIED="1653389813095" TEXT="Exploring price outliers on the botton of the list">
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1653313082750" ID="ID_512022868" MODIFIED="1653313164405" TEXT="The importance of a specific strip on our data set."/>
+</node>
+</node>
+</node>
+<node CREATED="1653303633852" ID="ID_1155102413" MODIFIED="1653392649270" TEXT="The date columns ">
+<icon BUILTIN="full-4"/>
+<node CREATED="1653313195106" ID="ID_117835141" MODIFIED="1653313667910" TEXT="registration_year&apos;.describe()"/>
+<node CREATED="1653313765643" ID="ID_1772704830" MODIFIED="1653390251964">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <h3 id="Text-to-datetime-format" style="font-family: Helvetica Neue, Helvetica, Arial, sans-serif; font-weight: bold; line-height: 1; color: rgb(0, 0, 0); margin-top: 0; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; font-size: 18.004px; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">
+      <font size="3">Text to datetime format </font>
+    </h3>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-4"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1653389941610" ID="ID_1916308470" MODIFIED="1653390306021" TEXT="distribution of values">
+<icon BUILTIN="full-4"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1653313842854" ID="ID_446301608" MODIFIED="1653313844037" TEXT="last_seen"/>
+<node CREATED="1653313825011" ID="ID_1935479398" MODIFIED="1653313832546" TEXT="date_crawled"/>
+<node CREATED="1653313833149" ID="ID_337685281" MODIFIED="1653313839029" TEXT="ad_created"/>
+</node>
+<node CREATED="1653389959802" ID="ID_903379684" MODIFIED="1653390411013" TEXT="upper and lower limit registration_year">
+<icon BUILTIN="full-4"/>
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1653390085764" ID="ID_241021442" MODIFIED="1653390159442" TEXT="table freq rehistration year"/>
+</node>
+</node>
+</node>
+<node CREATED="1653303649848" ID="ID_1095122067" MODIFIED="1653392649271" TEXT="Price by Brand">
+<icon BUILTIN="full-5"/>
+<node CREATED="1653303660013" ID="ID_1445791464" MODIFIED="1653303662196" TEXT="Average price per vehicle brand"/>
+<node CREATED="1653303663236" ID="ID_1384846970" MODIFIED="1653303671724" TEXT="Average number of kilometers per vehicle brand"/>
+<node CREATED="1653303685898" ID="ID_967743782" MODIFIED="1653303688389" TEXT="List of car brands by average price and average mileage"/>
+</node>
+<node CREATED="1653303694978" ID="ID_564086804" MODIFIED="1653391855350" TEXT="Storing Aggregate Data in a DataFrame">
+<icon BUILTIN="full-6"/>
+<node CREATED="1653303705870" ID="ID_1964407627" MODIFIED="1653390607038" TEXT="Identify categorical data">
+<icon BUILTIN="full-6"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1653303723119" ID="ID_1753239503" MODIFIED="1653390610446" TEXT="Finding the most common brand/model combi">
+<icon BUILTIN="full-6"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1653303734530" ID="ID_1321465971" MODIFIED="1653303736582" TEXT="Dividing the odometer in groups"/>
+</node>
+</node>
+<node CREATED="1653303748380" ID="ID_177300028" MODIFIED="1653390633326" TEXT="How much cheaper are damaged cars than their undamaged counterparts?">
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1653303758768" ID="ID_1757010909" MODIFIED="1653391968998" TEXT="Conclusions">
+<icon BUILTIN="full-8"/>
+</node>
+</node>
+</node>
+</map>
---
+++ 02 Data_Analisis_Visualization/README.md
@@ -0,0 +1,5 @@
+In this directory are the projects with which the following points have been worked:
+
+- Pandas and NumPy Fundamentals.
+- Data Visualization Fundamentals.
+- Storytelling Data Visualization and Information Design.
